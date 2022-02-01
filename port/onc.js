0fcedafe2ef696745b07b34bb1ba15d0ae9c5480
Bossgeeg <95701554+BoszGTec@users.noreply.github.com>
2022-02-01 18:18:18 +0700

    Create onc.js

---
+++ port/onc.js
@@ -0,0 +1,7 @@
+function onc(){
+    var email=document.getElementsByName("entry.774775268")[0];
+    var comment=document.getElementsByName("entry.364478082")[0];
+    email.value=""
+    comment.value=""
+    email=undefined ; comment=undefined ;
+}
