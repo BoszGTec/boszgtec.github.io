a4dd01869a1401147aac1994958441a82906efa5
Bossgeeg <95701554+BoszGTec@users.noreply.github.com>
2022-05-08 04:44:19 +0700

    Update script.js

--- test_space/HTML_SP/script/script.js
+++ test_space/HTML_SP/script/script.js
@@ -4,6 +4,16 @@ function Display_html() {
  var inp = document.getElementById("Input")
  var ct = document.getElementById("Content")
 
- ct.innerHTML=inp.value
+ var key = document.getElementById("Key")
+ var Rkey = "4r@PcrSJLt2=8jJsW-Gaw#F9-mGZ7a"
+
+ if( key.value == Rkey ) {
+  ct.innerHTML = inp.value
+ }
+ else {
+  ct.innerHTML = "invalid key"
+ }
+
 }
+
 
