3e60d6abdae1ce9809aad984055ef4d4a87af9c4
Bossgeeg <95701554+BoszGTec@users.noreply.github.com>
2022-05-09 15:24:58 +0700

    Update and rename resize-content.js to re-content.js

--- test_space/HTML_SP/script/re-content.js
+++ test_space/HTML_SP/script/re-content.js
@@ -1,5 +1,5 @@
 //input from : <input id="ct-height" style="width: 200px ;" type="range" class="form-range" min="100" max="1000" / >    
-function resize-ct(){
+function re-ct(){
  var ct = document.getElementById("Content")
  var ct-h = document.getElementById("ct-height")
  ct.style = " height : " + ct-h.value + "px ;"
