327b544b19d89741d6c6f4734a401343e2d0055a
Bossgeeg <95701554+BoszGTec@users.noreply.github.com>
2022-05-11 22:38:19 +0700

    Create check-rule.js

---
+++ test_space/HTML_SP/script/check-rule.js
@@ -0,0 +1,11 @@
+function c_rule(){
+ var inp_rule=document.getElementById("inp-rule")
+ var key = document.getElementById("Key")
+
+ if (inp_rule.checked){
+  Key.hidden = false
+ }
+
+ Display_html() 
+
+}
