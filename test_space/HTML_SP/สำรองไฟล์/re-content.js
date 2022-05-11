0b4b160e9d21c625a462927874f56292dcbe1220
Bossgeeg <95701554+BoszGTec@users.noreply.github.com>
2022-05-11 21:41:21 +0700

    Create re-content.js

---
+++ test_space/HTML_SP/สำรองไฟล์/re-content.js
@@ -0,0 +1,15 @@
+//input from : <input id="ct-height" style="width: 200px ;" type="range" class="form-range" min="100" max="1000" / >    
+function re_ct(){
+ var ct = document.getElementById("Content")
+ var ct_h = document.getElementById("ct-height")
+ ct.style.height = ct_h.value + "px"
+}
+
+
+
+
+
+
+
+
+
