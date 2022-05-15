c2fb6dcfa64da29121755278d55cc19c7b27da4c
Bossgeeg <95701554+BoszGTec@users.noreply.github.com>
2022-05-16 02:56:14 +0700

    Update dis-elem.js

--- test_space/HTML_SP/script/dis-elem.js
+++ test_space/HTML_SP/script/dis-elem.js
@@ -1,26 +1,32 @@
 // เรียกใช้โดย <input type="button" value="-" class="btn btn-primary" id="title-bt2"  onclick="" />
 
-function dis_html(){
- document.getElementById("Input").disabled = true 
- document.getElementById("dis-html").value = "+"
- document.getElementById("dis-html").onclick = "ava_html()"
+function dis_html(){ 
+ if(document.getElementById("dis-html").value != "+"){
+  document.getElementById("dis-html").value = "+"
+  document.getElementById("Input").disabled = true
+ }
+ else {
+ ava_html()
+ }
 }
 
 function ava_html(){
  document.getElementById("Input").disabled = false
  document.getElementById("dis-html").value = "-"
- document.getElementById("dis-html").onclick = "dis_html()"
 }
 
 function dis_css(){
- document.getElementById("Input-css").disabled = true 
- document.getElementById("dis-css").value = "+"
- document.getElementById("dis-css").onclick = "ava_css()"
+ if(document.getElementById("dis-css").value != "+"){
+  document.getElementById("Input-css").disabled = true 
+  document.getElementById("dis-css").value = "+"
+ }
+ else {
+  ava_css()
+ }
 }
 
 function ava_css(){
  document.getElementById("Input-css").disabled = false
  document.getElementById("dis-css").value = "-"
- document.getElementById("dis-css").onclick = "dis_css()"
 }
 
